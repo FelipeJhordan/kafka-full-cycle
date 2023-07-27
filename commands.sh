dd12c9b97afc3dcb60e35babf57deb6e83d19ef1
FelipeJhordan <felipejordan.alves@gmail.com>
2023-07-26 23:28:55 -0300

    feat: update command

--- commands.sh
+++ commands.sh
@@ -15,5 +15,11 @@ kafka-topics --bootstrap-server=localhost:9092 --topic=teste --describe
 # Criar consumer 
 kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste --from-beginning
 
+# Criar consumer e inseri-lo em um grupo.
+kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste --group=x
+
 # Criar Producer
 kafka-console-producer --bootstrap-server=localhost:9092 --topic=teste
+
+# Descrever CONSUMER GROUPS
+kafka-consumer-groups --bootstrap-server=localhost:9092 --group=x --describe
